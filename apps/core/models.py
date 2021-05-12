2c1ec13f946c470c52639cbb75594e405836b499
Robbie Mackay <rm@robbiemackay.com>
2021-05-13 11:23:27 +1200

    Add initial boilerplate

---
+++ apps/core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    class Meta:
+        get_latest_by = "updated_at"
+        ordering = ("-updated_at", "-created_at")
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
