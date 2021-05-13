b53a03ccf21381a8d7c8bac6dbd2ee24baa15e51
Robbie Mackay <rm@robbiemackay.com>
2021-05-14 10:28:04 +1200

    Working upload and conversion to images

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.files.storage import default_storage
+from pdf2image import convert_from_path
 
 
 class PitchUploadForm(forms.Form):
@@ -6,4 +8,13 @@ class PitchUploadForm(forms.Form):
 
     def process_pitch(self):
         # send email using the self.cleaned_data dictionary
-        pass
+        upload = self.cleaned_data["pitch_deck"]
+        dest = default_storage.open(upload.name, 'wb+')
+        for chunk in upload.chunks():
+            dest.write(chunk)
+        dest.close()
+
+        images = convert_from_path(dest.name)
+        for i, image in enumerate(images):
+            dest = default_storage.open(f"{upload.name}_{i}.png", "wb+")
+            image.save(fp=dest.file, format="png")
