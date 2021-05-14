26462bc1d414552214f4e7b220cc04c19304b35d
Robbie Mackay <rm@robbiemackay.com>
2021-05-14 15:50:55 +1200

    Redirect to upload page if no pitch decks yet

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
 
@@ -7,14 +9,24 @@ from .models import PitchDeck
 
 
 class PitchViewer(DetailView):
-    def get_object(self, **kwargs):
-        return PitchDeck.objects.all().latest("created")
+    def get_object(self, *args, **kwargs):
+        try:
+            return PitchDeck.objects.all().latest("created")
+        except PitchDeck.DoesNotExist:
+            return None
+
+    def get(self, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object is None:
+            return redirect("upload")
+        else:
+            return super().get(*args, **kwargs)
 
 
 class PitchUploadView(FormView):
     template_name = "core/pitchdeck_upload.html"
     form_class = PitchUploadForm
-    success_url = "/"
+    success_url = reverse_lazy("home")
 
     def form_valid(self, form):
         form.process_pitch()
