76700fd8d8f77d0f58a2f7713cdd2f9349b63429
Robbie Mackay <rm@robbiemackay.com>
2021-05-14 15:27:21 +1200

    Move pdf conversion into background task

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -1,26 +1,15 @@
-from io import BytesIO
-
 from django import forms
-from django.core.files.base import ContentFile
-from pdf2image import convert_from_path
 
 from .models import PitchDeck
-from .models import PitchImage
+from .tasks import convert_pitch_to_image
 
 
 class PitchUploadForm(forms.Form):
     pitch_deck = forms.FileField(required=True)
 
     def process_pitch(self):
-        # send email using the self.cleaned_data dictionary
+        # Save uploaded image
         upload = self.cleaned_data["pitch_deck"]
         pitch = PitchDeck.objects.create(name=upload.name, original=upload)
-
-        images = convert_from_path(pitch.original.path, fmt="png", size=(800, None))
-        for page, image in enumerate(images):
-            pitch_image = PitchImage(pitch_deck=pitch, page=page)
-            # dest = default_storage.open(f"{upload.name}_{i}.png", "wb+")
-            output_file = BytesIO()
-            image.save(fp=output_file, format="png")
-            pitch_image.image.save(f"{pitch.original.name}.{page}.png", ContentFile(output_file.getvalue()), save=False)
-            pitch_image.save()
+        # Queue conversion to images
+        convert_pitch_to_image.delay(pitch.id)
