53296916f49a9b3752f452ebc1d5557b3b59d582
Robbie Mackay <rm@robbiemackay.com>
2021-05-14 15:50:55 +1200

    Redirect to upload page if no pitch decks yet

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from .views import PitchUploadView
 from .views import PitchViewer
 
-urlpatterns = [path("", PitchViewer.as_view()), path("upload", PitchUploadView.as_view())]
+urlpatterns = [path("", PitchViewer.as_view(), name="home"), path("upload", PitchUploadView.as_view(), name="upload")]
